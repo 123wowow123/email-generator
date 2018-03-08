a273602c326176333603cdd0c9417f3205954e08
wowow <flynni2008@gmail.com>
2018-03-07 20:51:01 -0800

    wip

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,12 +4,19 @@ import { Config } from '../type';
 console.log('Loading config for (NODE_ENV):', process.env.NODE_ENV);
 
 let environment: string | undefined = process.env.NODE_ENV || 'development';
-environment =
-  process.env.NODE_ENV === 'test' ? 'development' : process.env.NODE_ENV;
-
-const shared = require('./shared') || {};
-const envSpecific = require('./' + environment) || {};
+environment = process.env.NODE_ENV === 'test' ? 'development' : process.env.NODE_ENV;
 
 const all = {};
+let envSpecific;
+const shared = require('./shared') || {};
+
+if (environment === 'development') {
+    envSpecific = require('./' + environment);
+}
+else {
+    envSpecific = {};
+}
+
+
 
 export default _.merge(all, shared.default, envSpecific.default) as Config;
